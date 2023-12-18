3b8f810c5b7b817fd52bcdab7dfed7e938fb204c
Константин Трохимец <katrohimets@mail.ru>
2023-12-18 16:55:55 +0300

    add style

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,9 +1,22 @@
+import Styles from './app.module.css'
 
-
-function App() {
+const App = () =>{
   return (
-    <div className="App">
-     Я реакт приложение
+    <div className={Styles['wrapper']}>
+    <header>
+      <a href="">Ссылка 1</a>
+      <a href="">Ссылка 2</a>
+      <a href="">Ссылка 3</a>
+      <a href="">Ссылка 4</a>
+    </header>
+    <aside>Я сайдбар</aside>
+    <main>
+      <div>
+        <img src=""/>
+        <h2>Я карточка 1</h2>
+        <p>Описание</p>
+      </div>
+    </main>
     </div>
   );
 }
